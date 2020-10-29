e5ceef5f9180ff934a523afe743f62e944b91915
sanchezd90 <sanchezd90@gmail.com>
2020-10-29 00:06:42 -0300

    Modifico colecciones destino
    Ahora tiene dos funciones. Una para subir evaluaciones y otra para subir informes a sus respectivas colecciones. Antes solo subia evaluaciones porque solo habia una colección.

--- jsonupload.py
+++ jsonupload.py
@@ -5,23 +5,38 @@ import os
 cluster=pymongo.MongoClient("mongodb+srv://sanchezd90:dbuser-L6H6@cluster0.wwnbb.mongodb.net/?retryWrites=true&w=majority")
 
 db=cluster["ENPS"]
-col=db["evaluaciones"]
-codigos=[x["codigo"]+".json" for x in col.find({},{"_id":0,"codigo":1}) ]
 
-def add_new():
-    #recorro los archivos que hay en la carpeta local de colecciones
-    for x in os.listdir("./colecciones"):
+def add_new_ev():
+    col=db["evaluaciones"]
+    codigos=[x["codigo"]+".json" for x in col.find({},{"_id":0,"codigo":1}) ]
+
+    #recorro los archivos que hay en la carpeta local de evaluacionesjson
+    for x in os.listdir("./evaluacionesjson"):
         #si no está ya subido en mongo lo subo
         if x not in codigos:
             diccionario=None
             name=x
-            path="./colecciones/"
+            path="./evaluacionesjson/"
             file_path=os.path.join(path,name)
             with open(file_path,"r") as f:
                 diccionario=json.load(f)
             y=col.insert_one(diccionario)
 
-add_new()
+def add_new_ev():
+    col=db["informes"]
+    codigos=[x["codigo"]+".json" for x in col.find({},{"_id":0,"codigo":1}) ]
+
+    #recorro los archivos que hay en la carpeta local de informesjson
+    for x in os.listdir("./informesjson"):
+        #si no está ya subido en mongo lo subo
+        if x not in codigos:
+            diccionario=None
+            name=x
+            path="./informesjson/"
+            file_path=os.path.join(path,name)
+            with open(file_path,"r") as f:
+                diccionario=json.load(f)
+            y=col.insert_one(diccionario)
 
 
 
