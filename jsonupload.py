ec50a248aa90f8c352fbafb5d0864e4b7682cfe7
sanchezd90 <sanchezd90@gmail.com>
2020-10-30 15:26:39 -0300

    Update jsonupload.py
    renombro las funciones y agrego que al finalizar el proceso borre los documentos de la carpeta correspondiente (evaluaciones o informes)

--- jsonupload.py
+++ jsonupload.py
@@ -6,39 +6,42 @@ cluster=pymongo.MongoClient("mongodb+srv://sanchezd90:dbuser-L6H6@cluster0.wwnbb
 
 db=cluster["ENPS"]
 
-def add_new_ev():
+def add_new_eval():
     col=db["evaluaciones"]
     codigos=[x["codigo"]+".json" for x in col.find({},{"_id":0,"codigo":1}) ]
 
     #recorro los archivos que hay en la carpeta local de evaluacionesjson
     for x in os.listdir("./evaluacionesjson"):
+        name=x
+        path="./evaluacionesjson/"
+        file_path=os.path.join(path,name)
         #si no está ya subido en mongo lo subo
         if x not in codigos:
             diccionario=None
-            name=x
-            path="./evaluacionesjson/"
-            file_path=os.path.join(path,name)
             with open(file_path,"r") as f:
                 diccionario=json.load(f)
             y=col.insert_one(diccionario)
+        os.remove(file_path)
 
-def add_new_ev():
+def add_new_info():
     col=db["informes"]
     codigos=[x["codigo"]+".json" for x in col.find({},{"_id":0,"codigo":1}) ]
 
     #recorro los archivos que hay en la carpeta local de informesjson
     for x in os.listdir("./informesjson"):
+        name=x
+        path="./informesjson/"
+        file_path=os.path.join(path,name)
         #si no está ya subido en mongo lo subo
         if x not in codigos:
             diccionario=None
-            name=x
-            path="./informesjson/"
-            file_path=os.path.join(path,name)
             with open(file_path,"r") as f:
                 diccionario=json.load(f)
             y=col.insert_one(diccionario)
+        os.remove(file_path)
 
-
+add_new_info()
+add_new_eval()
 
 
 
