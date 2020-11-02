ede8b13b5ba000e5e037cefd695f37cf334f595c
sanchezd90 <sanchezd90@gmail.com>
2020-11-01 22:27:29 -0300

    Update lector.py
    Le agregue un try except para que cuando encuentre algun error en la lectura de un csv o docx lo deje y siga con el siguiente

--- lector.py
+++ lector.py
@@ -57,10 +57,13 @@ class Lector():
 
         #loop para revisar archivos docs y agregar datos a datos_informes
         for archivo in os.listdir('informesdocx/'):
-            if archivo.endswith(".docx"):
-                archivo=os.path.join("./informesdocx/",archivo)
-                getText(archivo)
-                os.remove(archivo)
+            try:
+                if archivo.endswith(".docx"):
+                    archivo=os.path.join("./informesdocx/",archivo)
+                    getText(archivo)
+                    os.remove(archivo)
+            except Exception:
+                pass
 
         with open("informes.json","w") as f:
             json.dump(diccionarioInformes,f,indent=4)
@@ -82,35 +85,38 @@ class Lector():
 
         #loop para armar diccionarios con la información de cada informe. La info va a diccionarioInformes
         for informe in datos_evaluaciones:
-            # informe[1][11] es la casilla en la que está la fecha de evaluacion
-            split=informe[1][11].split("/")
-            for i,s in enumerate(split):
-                if len(s)<2:
-                    split[i]="0"+split[i]
-            if len(split[1])==1:
-                split[1]="0"+split[1]
-            if len(split[0])==1:
-                split[0]="0"+split[0]
-            fecha=split[2]+"-"+split[1]+"-"+split[0]
-            dni=informe[1][12].replace(".","").replace(",","")
-            codigo = dni+"-"+fecha
-            if codigo not in diccionarioEvaluaciones:
-                diccionarioEvaluaciones[codigo]={}
-                #inicio un loop para explorar las celdas. comienzo explorando las filas 
-                for x in range(len(informe)):
-                    #me fijo si la primera celda esta completa, así puedo saber que es fila de etiquetas
-                    if len(informe[x][0]) > 0:
-                        #armo el key del diccionario que va a llevar el nombre de la fila
-                        key = informe[x][0]
-                        #agrego al diccionario una key por prueba con el valor vacio
-                        diccionarioEvaluaciones[codigo][key]={}
-                        #inicio un loop para enumerar los datos de la fila de abajo de la fila que tiene las etiquetas
-                        for index,item in enumerate(informe[x+1]):
-                            #exploro si cada celda tiene un valor
-                            if index >0:
-                                #si la celda tiene valor actualizo el diccionario con la etiqueta como key y el valor como value
-                                k=informe[x][index]
-                                diccionarioEvaluaciones[codigo][key][k]=item
+            try:
+                # informe[1][11] es la casilla en la que está la fecha de evaluacion
+                split=informe[1][11].split("/")
+                for i,s in enumerate(split):
+                    if len(s)<2:
+                        split[i]="0"+split[i]
+                if len(split[1])==1:
+                    split[1]="0"+split[1]
+                if len(split[0])==1:
+                    split[0]="0"+split[0]
+                fecha=split[2]+"-"+split[1]+"-"+split[0]
+                dni=informe[1][12].replace(".","").replace(",","")
+                codigo = dni+"-"+fecha
+                if codigo not in diccionarioEvaluaciones:
+                    diccionarioEvaluaciones[codigo]={}
+                    #inicio un loop para explorar las celdas. comienzo explorando las filas 
+                    for x in range(len(informe)):
+                        #me fijo si la primera celda esta completa, así puedo saber que es fila de etiquetas
+                        if len(informe[x][0]) > 0:
+                            #armo el key del diccionario que va a llevar el nombre de la fila
+                            key = informe[x][0]
+                            #agrego al diccionario una key por prueba con el valor vacio
+                            diccionarioEvaluaciones[codigo][key]={}
+                            #inicio un loop para enumerar los datos de la fila de abajo de la fila que tiene las etiquetas
+                            for index,item in enumerate(informe[x+1]):
+                                #exploro si cada celda tiene un valor
+                                if index >0:
+                                    #si la celda tiene valor actualizo el diccionario con la etiqueta como key y el valor como value
+                                    k=informe[x][index]
+                                    diccionarioEvaluaciones[codigo][key][k]=item
+            except Exception:
+                pass
 
         with open("evaluaciones.json","w") as f:
             json.dump(diccionarioEvaluaciones,f,indent=4)
@@ -119,6 +125,6 @@ class Lector():
 #para crear instancia y leer datos de ambos json
 
 lector=Lector()
-#lector.leer_evaluaciones()
-lector.leer_informes()
+lector.leer_evaluaciones()
+#lector.leer_informes()
 
